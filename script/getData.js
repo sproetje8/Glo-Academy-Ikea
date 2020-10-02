ecd6ae6ae28e238d34a44d9723f1185a95ad1615
Evelien Kulikova <Evelien_Kulikova@epam.com>
2020-10-02 12:15:35 +0300

    feat: Add code of day 2 and homework 2

--- script/getData.js
+++ script/getData.js
@@ -1,8 +1,74 @@
+const PARAM = {
+	cat: 'category',
+	subcat: 'subcategory',
+	search: ['name', 'description', 'category', 'subcategory'],
+};
+
 export const getData = {
 	url: 'database/dataBase.json',
 	get(process) {
-		fetch(this.url);
+		fetch(this.url)
+			.then((response) => response.json().then(process))
+			.catch();
+	},
+	wishList(list, callback) {
+		this.get((data) => {
+			const result = data.filter((item) => list.includes(item.id));
+			callback(result);
+		});
+	},
+	item(value, callback) {
+		this.get((data) => {
+			const result = data.find((item) => item.id === value);
+			callback(result);
+		});
+	},
+	cart(list, callback) {
+		this.get((data) => {
+			const result = data.filter((item) =>
+				list.some((obj) => obj.id === item.id)
+			);
+			callback(result);
+		});
+	},
+	category(prop, value, callback) {
+		this.get((data) => {
+			const result = data.filter(
+				(item) => item[PARAM[prop]].toLowerCase() === value.toLowerCase()
+			);
+			callback(result);
+		});
+	},
+	search(value, callback) {
+		this.get((data) => {
+			const result = data.filter((item) => {
+				for (const prop in item) {
+					if (
+						PARAM.search.includes(prop) &&
+						item[prop].toLowerCase().includes(value.toLowerCase())
+					) {
+						return true;
+					}
+				}
+			});
+			callback(result);
+		});
+	},
+	catalog(callback) {
+		this.get((data) => {
+			const result = Array.from(new Set(data.map((item) => item.category)));
+			callback(result);
+		});
+	},
+	subCatalog(value, callback) {
+		this.get((data) => {
+			const filteredByValue = data.filter(
+				(item) => item.category.toLowerCase() === value.toLowerCase()
+			);
+			
+			const result = Array.from(new Set(filteredByValue.map((item) => item.subcategory)));
+
+			callback(result);
+		});
 	},
 };
-
-getData.get(console.log);
--- script/loadData.js
+++ script/loadData.js
@@ -1,32 +1,46 @@
-// import { getData } from './getData.js';
+import { getData } from './getData.js';
 
-// const wishlist = ['idd005', 'idd100', 'idd088', 'idd018'];
+const wishlist = ['idd005', 'idd100', 'idd088', 'idd018'];
 
-// const cartList = [
-// 	{
-// 		id: 'idd015',
-// 		count: 3,
-// 	},
-// 	{
-// 		id: 'idd035',
-// 		count: 1,
-// 	},
-// 	{
-// 		id: 'idd025',
-// 		count: 2,
-// 	},
-// ];
+const cartList = [
+	{
+		id: 'idd015',
+		count: 3,
+	},
+	{
+		id: 'idd035',
+		count: 1,
+	},
+	{
+		id: 'idd025',
+		count: 2,
+	},
+];
 
 export const loadData = () => {
 	if (location.search) {
-		console.log('search');
+		const search = decodeURI(location.search);
+		const prop = search.split('=')[0].substring(1);
+		const value = search.split('=')[1];
+
+		if (prop === 's') {
+			getData.search(value, (data) => console.log(data));
+		} else if (prop === 'wishlist') {
+			getData.wishList(wishList, (data) => console.dir({ wishlist: data }));
+		} else if (prop === 'cat' || prop === 'subcat') {
+			getData.category(prop, value, (data) => console.log(data));
+		}
 	}
 
 	if (location.hash) {
-		console.log(location.hash);
+		getData.item(location.hash.substring(1), (data) => console.log(data));
 	}
 
 	if (location.pathname.includes('cart')) {
-		console.log('cart');
+		getData.cart(cartList, (data) => console.log(data));
 	}
+
+	getData.catalog((data) => console.log(data));
+
+	getData.subCatalog("Декор", (data) => console.log(data));
 };
